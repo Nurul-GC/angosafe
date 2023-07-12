9ff25253d76308e7325e55d4291ec2b52c620c20
Nuninha-GC <nuruldecarvalho@gmail.com>
2023-07-12 14:25:17 +0100

    update index.py

---
+++ index.py
@@ -0,0 +1,10 @@
+"""
+script principal do projecto
+para executa-lo basta navegar ate a localização atual do script
+e digitar no terminal:
+*** linux ***
+    ~$: python3 ./index.py
+*** windows ***
+    C:/localizacao/do/script/> py index.py
+"""
+
