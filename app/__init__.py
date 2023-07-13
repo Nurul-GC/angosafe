4a90055585b2f38d5c83c9939354772e1d2f7b63
Nuninha-GC <nuruldecarvalho@gmail.com>
2023-07-13 12:43:37 +0100

    update app/*

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,17 @@
 """iniciando a aplicacao..."""
 
-# ************************************************
-#  (c) 2019-2021 Nurul-GC                        *
-# ************************************************
-
 from app.config import Config
-from flask import Blueprint, Flask, render_template, make_response, request, jsonify, redirect
-from flask_sqlalchemy import SQLAlchemy
+from flask import Blueprint, Flask
+# from flask_sqlalchemy import SQLAlchemy
 
 # iniciando a instancia da aplicacao
 app = Flask(__name__)
+blueprint = Blueprint("angosafe", __name__)
 
 # aplicando as configuracoes ao projecto
 app.config.from_object(Config)
 
 # iniciando a instancia da base dados
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
+
+from app import routes
