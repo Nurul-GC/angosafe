ca880319aa4d5e7bb50eb796cf7d5084eec99177
Nuninha-GC <nuruldecarvalho@gmail.com>
2023-08-03 10:57:26 +0100

    update app/routes/*

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,7 @@
 """defining our routes"""
 
+import json
+import sys
 from app import app
 from flask import render_template, request, jsonify, redirect, url_for
 from gcrypter import encrypt, decrypt
@@ -16,10 +18,13 @@ def encryption():
     """pagina encriptacao"""
     if request.query_string:
         text = request.query_string.decode().split('=')[1]
-        return jsonify(encrypted=encrypt(text=text))
+        text_json = {"encrypted":encrypt(text=text)}
+        return json.dumps(text_json, indent=4, ensure_ascii=False) 
     elif request.method == 'POST':
         text = request.form.to_dict()['text']
-        return jsonify(encrypted=encrypt(text=text))
+        sys.set_int_max_str_digits(1000000)
+        text_json = {"encrypted":encrypt(text=text)}
+        return json.dumps(text_json, indent=4, ensure_ascii=False) 
     return render_template("encrypt.html")
 
 
@@ -31,29 +36,33 @@ def decryption():
             values = request.query_string.decode().split('&')
             value1 = int(values[0].split('=')[1])
             value2 = int(values[1].split('=')[1])
-            return jsonify(decrypted=decrypt(text_enc=(value1, value2)))
+            text_json = {"decrypted":decrypt(text_enc=(value1, value2))}
+            return json.dumps(text_json, indent=4, ensure_ascii=False) 
         else:
             values = request.query_string.decode().split('=')[1].split(',')
             value1 = int(values[0][1:])
             value2 = int(values[1][:-1])
-            return jsonify(decrypted=decrypt(text_enc=(value1, value2)))
+            text_json = {"decrypted":decrypt(text_enc=(value1, value2))}
+            return json.dumps(text_json, indent=4, ensure_ascii=False) 
     elif request.method == 'POST':
         values = request.form.to_dict()
         if values['value1']!='' and values['value2']!='':
             value1 = int(values['value1'])
             value2 = int(values['value2'])
-            return jsonify(decrypted=decrypt(text_enc=(value1, value2))) 
+            text_json = {"decrypted":decrypt(text_enc=(value1, value2))}
+            return json.dumps(text_json, indent=4, ensure_ascii=False) 
         else:
             both_values = values['values'].split(',')
             value1 = both_values[0]
             value2 = both_values[1]
-            if '('in value1 and ')' in value2:
+            if '(' in value1 and ')' in value2:
                 value1 = int(value1[1:])
                 value2 = int(value2[:-1])
             else:
                 value1 = int(value1)
                 value2 = int(value2)
-            return jsonify(decrypted=decrypt(text_enc=(value1, value2))) 
+            text_json = {"decrypted":decrypt(text_enc=(value1, value2))}
+            return json.dumps(text_json, indent=4, ensure_ascii=False) 
     return render_template("decrypt.html")
 
 
