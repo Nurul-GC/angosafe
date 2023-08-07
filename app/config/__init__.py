326c40d8561048d4245842d24aa0dcf924cd6a35
Nuninha-GC <nuruldecarvalho@gmail.com>
2023-08-07 10:57:29 +0100

    update app/config/*

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -7,13 +7,13 @@ from secrets import token_hex
 from gcrypter import decrypt
 
 # definindo a localizacao da base-dados
-basedados_dir = os.path.abspath(os.path.dirname(__file__) + '/database')
+# basedados_dir = os.path.abspath(os.path.dirname(__file__) + '/database')
 
 
 class Config(object):
     """classe objecto com as configuracoes da app"""
     SECRET_KEY = os.environ.get('SECRET_KEY') or decrypt((3569661159, 3886674852)) or token_hex(32)
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///'+os.path.join(basedados_dir, 'angosafe.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    DEBUG = True
-    RELOAD = True
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///'+os.path.join(basedados_dir, 'angosafe.db')
+    # SQLALCHEMY_TRACK_MODIFICATIONS = True
+    DEBUG = False
+    RELOAD = False
